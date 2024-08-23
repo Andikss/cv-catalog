5323dfb2669648440d9eaeb67e23ff0588cdb405
Andikss <andikadwisaputra.dev@gmail.com>
2024-08-23 11:33:04 +0700

    feat: add react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+import { lazy, Suspense } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { Progress } from '@chakra-ui/react';
+
+const Home = lazy(() => import('./Pages/Home.jsx'));
+
 const App = () => {
   return (
-    <div>App</div>
-  )
+    <Suspense fallback={<Progress size="xs" isIndeterminate />}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Suspense>
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
 import { StrictMode } from 'react'
+import { BrowserRouter as Router } from 'react-router-dom';
 import { ChakraProvider } from '@chakra-ui/react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
+    <Router>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </Router>
   </StrictMode>,
 )
