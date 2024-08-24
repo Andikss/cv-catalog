ef3bb7e974b664b9e920dc5c163176d48e2f1129
Andikss <andikadwisaputra.dev@gmail.com>
2024-08-24 08:05:18 +0700

    feat: counting stats

--- src/Components/Molecules/Stat/Stat.jsx
+++ src/Components/Molecules/Stat/Stat.jsx
@@ -1,11 +1,32 @@
-import { Center, Text } from '@chakra-ui/react'
-import PropTypes from 'prop-types'
+import { Center, Text } from '@chakra-ui/react';
+import PropTypes from 'prop-types';
+import { useState, useEffect } from 'react';
 
-export const Stat = ({total, title, description}) => {
+export const Stat = ({ total, title, description }) => {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    let start = 0;
+    const end = parseInt(total, 10);
+    if (start === end) return;
+
+    const duration = 100;
+    const stepTime = Math.abs(Math.floor(duration / end));
+    let current = start;
+    const increment = () => {
+      current += 1;
+      setCount(current);
+      if (current < end) {
+        setTimeout(increment, stepTime);
+      }
+    };
+    increment();
+  }, [total]);
+
   return (
     <Center flexDirection="column">
       <Text color="teal" fontSize="xxx-large" fontWeight="600" mb={2}>
-        {total}+
+        {count}+
       </Text>
       <Text fontWeight="550" fontSize="large">
         {title}
@@ -14,11 +35,11 @@ export const Stat = ({total, title, description}) => {
         {description}
       </Text>
     </Center>
-  )
-}
+  );
+};
 
 Stat.propTypes = {
-    total: PropTypes.string.isRequired,
-    title: PropTypes.string.isRequired,
-    description: PropTypes.string.isRequired,
-}
+  total: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+};
