478c6137d1e2122f4d312e8e8381bd1a024f6cda
Andikss <andikadwisaputra.dev@gmail.com>
2024-08-28 16:23:14 +0700

    feat: create pwa service worker

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,32 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'DesainCV',
+        short_name: 'DesainCV',
+        description: 'Desain CV Toploker',
+        theme_color: '#344767',
+        icons: [
+          {
+            src: '/assets/favicon-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/assets/favicon-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
   resolve: {
     alias: {
       '@': '/src',
