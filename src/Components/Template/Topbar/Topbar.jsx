1dfd9fc4d8ec50ecfa9a5713b0523b64bd98461e
Andikss <andikadwisaputra.dev@gmail.com>
2024-08-28 09:31:54 +0700

    fix: topbar code layout

--- src/Components/Template/Topbar/Topbar.jsx
+++ src/Components/Template/Topbar/Topbar.jsx
@@ -16,15 +16,13 @@ export const Topbar = () => {
   };
 
   return (
-    <Flex as="header" position="fixed" top="24px" left="50%" transform="translateX(-50%)" width="94%" bg="rgba(240, 240, 240, 0.8)" color="#344767" py={4} px={8} boxShadow="md" zIndex="1000" borderRadius="15px" flexDirection="column" alignItems="center">
-      <Flex width="100%" justifyContent="space-between" alignItems="center">
-        <Flex justifyContent="space-between" alignItems="center" display="flex" width="100%">
-          <Logo/>
-          <Flex direction="row" alignItems="center" gap={3}>
-            <Catalog scroll={handleSmoothScroll}/>
-            <Company scroll={handleSmoothScroll}/>
-            <Developer/>
-          </Flex>
+    <Flex as="header" position="fixed" top="24px" left="50%" transform="translateX(-50%)" width="94%" bg="rgba(240, 240, 240, 0.8)" color="#344767" py={2} pl={2} pr={4} boxShadow="md" zIndex="1000" borderRadius="100px" flexDirection="column" alignItems="center">
+      <Flex justifyContent="space-between" alignItems="center" display="flex" width="100%">
+        <Logo/>
+        <Flex direction="row" alignItems="center" gap={5}>
+          <Catalog scroll={handleSmoothScroll}/>
+          <Company scroll={handleSmoothScroll}/>
+          <Developer/>
         </Flex>
       </Flex>
     </Flex>
