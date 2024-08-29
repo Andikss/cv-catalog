f0ba4c12aa6408245bf5fb057c5793bd033f3536
Andikss <andikadwisaputra.dev@gmail.com>
2024-08-29 09:38:28 +0700

    feat: display pricelist on popup

--- src/Features/Home/Popup/Popup.jsx
+++ src/Features/Home/Popup/Popup.jsx
@@ -11,27 +11,17 @@ export const Popup = () => {
   return (
     <Modal isOpen={isOpen} onClose={onClose} size="md" isCentered>
       <ModalOverlay />
-      <ModalContent
-        width="70dvh"
-        height="70dvh"
-        maxWidth="70dvh"
-        maxHeight="70dvh"
-        borderRadius="md"
-        p={0}
-      >
-        <ModalCloseButton  zIndex={999}/>
-        <ModalBody p={0} display="flex" flexDirection="column" justifyContent="center" alignItems="center">
+      <ModalContent borderRadius="md" p={0}>
+        <ModalCloseButton zIndex={999} />
+        <ModalBody p={0} display="flex" justifyContent="center" alignItems="center">
           <Image
-          src="https://images.unsplash.com/photo-1521747116042-5a810fda9664?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzNjUyOXwwfDF8c2VhcmNofDJ8fGltYWdlfGVufDB8fHx8MTY4Nzg4NzE0&ixlib=rb-1.2.1&q=80&w=1080"
-          alt="Promo"
-          position="absolute"
-          top={0}
-          left={0}
-          width="100%"
-          height="100%"
-          objectFit="cover"
-          borderRadius="md"
-        />
+            src="assets/pricelist.png"
+            alt="Promo"
+            borderRadius="md"
+            objectFit="cover"
+            maxWidth="100%"
+            maxHeight="80dvh"
+          />
         </ModalBody>
       </ModalContent>
     </Modal>
