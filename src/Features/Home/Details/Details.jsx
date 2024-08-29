69c23d17524500fd72acbbf4544e26f8f0f389bf
Andikss <andikadwisaputra.dev@gmail.com>
2024-08-29 16:31:28 +0700

    fix: details transition

--- src/Features/Home/Details/Details.jsx
+++ src/Features/Home/Details/Details.jsx
@@ -20,10 +20,10 @@ const floatingAnimation = {
 };
 
 const detailVariants = {
-  hidden: { opacity: 0, x: "100%" },
+  hidden: { opacity: 0, y: "100%" },
   visible: (index) => ({
     opacity: 1,
-    x: "0%",
+    y: "0%",
     transition: {
       delay: index * 0.1,
       duration: 0.6,
